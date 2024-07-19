45d7bca85b6555c14d535d425ed84e6c23e99926
Kianoush-Azarnia <kianoushazarnia@gmail.com>
2024-07-19 19:36:18 +0330

    Kelvin

--- ch2/s2c4_cf.go
+++ ch2/s2c4_cf.go
@@ -20,11 +20,13 @@ func main() {
 		c := Celsius(t)
 
 		fmt.Printf("%v = %v, %v = %v\n", f, FToC(f), c, CToF(c))
+		fmt.Printf("%v = %v, %v = %v\n", f, FToK(f), c, CToK(c))
 	}
 }
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 func CToF(c Celsius) Fahrenheit {
 	return Fahrenheit(c*9/5 + 32)
@@ -34,10 +36,22 @@ func FToC(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
 }
 
+func FToK(f Fahrenheit) Kelvin {
+	return Kelvin((f-32)*5/9 + 273.15)
+}
+
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c + 273.15)
+}
+
 func (c Celsius) String() string {
 	return fmt.Sprintf("%g°C", c)
 }
 
 func (f Fahrenheit) String() string {
 	return fmt.Sprintf("%g°F", f)
+}
+
+func (k Kelvin) String() string {
+	return fmt.Sprintf("%.2f°K", k)
 }
