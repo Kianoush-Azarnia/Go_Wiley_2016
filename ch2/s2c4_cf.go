b2306396fb20b4f2399d574d6a23a8b2b26bbfcc
Kianoush-Azarnia <kianoushazarnia@gmail.com>
2024-07-19 17:17:49 +0330

    cf

---
+++ ch2/s2c4_cf.go
@@ -0,0 +1,43 @@
+// Cf converts its numeric argument to Celsius and Fahrenheit.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+
+		f := Fahrenheit(t)
+		c := Celsius(t)
+
+		fmt.Printf("%v = %v, %v = %v\n", f, FToC(f), c, CToF(c))
+	}
+}
+
+type Celsius float64
+type Fahrenheit float64
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%g°F", f)
+}
