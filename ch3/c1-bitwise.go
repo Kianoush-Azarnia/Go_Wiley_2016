80e0ac5c459413bf552fdf6b49c7226118050c86
Kianoush-Azarnia <kianoushazarnia@gmail.com>
2024-07-20 17:30:53 +0330

    bitwise

---
+++ ch3/c1-bitwise.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x uint8 = 1<<1 | 1<<5
+	var y uint8 = 1<<1 | 1<<2
+
+	fmt.Printf("%08b\n", x) // set {1, 5}
+	fmt.Printf("%08b\n", y) // set {1, 2}
+
+	fmt.Printf("%08b\n", x&y)
+	fmt.Printf("%08b\n", x|y)
+	fmt.Printf("%08b\n", x^y)
+	fmt.Printf("%08b\n", x&^y)
+
+	for i := uint8(0); i < 8; i++ {
+		if x&(1<<i) != 0 {
+			fmt.Println(i) // "1", "5"
+		}
+	}
+
+	fmt.Printf("%08b\n", x<<1) // set {2, 6}
+	fmt.Printf("%08b\n", x>>1) // set {0, 4}
+}
