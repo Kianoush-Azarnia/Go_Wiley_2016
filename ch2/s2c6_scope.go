ee31994f4ddb4c890bb0ea06ec771e8f6e7a1075
Kianoush-Azarnia <kianoushazarnia@gmail.com>
2024-07-20 13:36:48 +0330

    scope x

--- ch2/s2c6_scope.go
+++ ch2/s2c6_scope.go
@@ -7,7 +7,8 @@ func main() {
 	for i := 0; i < len(x); i++ {
 		x := x[i]
 		if x != '!' {
-			x := x + 'A' - 'a'  // makes uppercase?
+			x := x + 'A' - 'a' // makes uppercase?
+			// book: above ie not equivalent to unicode.ToUpper
 			fmt.Printf("%c", x) // HELLO (one char per iterate)
 		}
 	}
