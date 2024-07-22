839f864dadb6e861572dc725788e0c0a1f33b358
Kianoush-Azarnia <kianoushazarnia@gmail.com>
2024-07-23 00:53:50 +0330

    color.RGBA

--- ch3/c5-mandelbrot.go
+++ ch3/c5-mandelbrot.go
@@ -52,7 +52,7 @@ func mandelbrot(z complex128) color.Color {
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+			return color.RGBA{10 + contrast*n, 50 + contrast*n, 80 + contrast*n, 255 - contrast*n}
 		}
 	}
 	return color.Black
